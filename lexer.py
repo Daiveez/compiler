61a5f93ef627bd0db9a98ec5fed3d2fa0ebc790b
Chiamaka  Anyanwu <93395990+grace2709@users.noreply.github.com>
2024-10-24 13:57:01 +0100

    Updated lexer.py to add OOP

--- lexer.py
+++ lexer.py
@@ -1,7 +1,18 @@
 import ply.lex as lex
+from rply import LexerGenerator
+# adding Reserved words
+reserved = {
+   'if' : 'IF',
+   'then' : 'THEN',
+   'else' : 'ELSE',
+   'while' : 'WHILE',
+    'for' : 'FOR',
+    'do' : 'DO',
+    'break' : 'BREAK',
+}
 
 # List of token names
-tokens = (
+tokens = [
     'NUMBER',
     'PLUS',
     'MINUS',
@@ -11,7 +22,9 @@ tokens = (
     'RPAREN',
     'ID',
     'ASSIGN',
-)
+    'COMMENT',
+    'CCODE',
+] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_PLUS = r'\+'
@@ -28,9 +41,10 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
-# Define a rule for identifiers
+# Define a rule for identifiers and reserved words
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
     return t
 
 # Define a rule so we can track line numbers
@@ -46,6 +60,59 @@ def t_error(t):
     print(f"Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
 
+# Declare the state
+states = (
+    ('comment', 'exclusive'),
+)
+
+# Match the first { and enter the comment state
+def t_comment(t):
+    r'\{'
+    t.lexer.code_start = t.lexer.lexpos
+    t.lexer.level = 1
+    t.lexer.begin('comment')
+
+# Rules for the comment state
+def t_comment_lbrace(t):
+    r'\{'
+    t.lexer.level += 1
+
+def t_comment_rbrace(t):
+    r'\}'
+    t.lexer.level -= 1
+
+    if t.lexer.level == 0:
+        t.value = t.lexer.lexdata[t.lexer.code_start:t.lexer.lexpos+1]
+        t.type = 'COMMENT'
+        t.lexer.lineno += t.value.count('\n')
+        t.lexer.begin('INITIAL')
+        return t
+
+# C or C++ comment (ignore)
+def t_comment_C(t):
+    r'(/\*(.|\n)*?\*/)|(//.*)'
+    pass
+
+# C string
+def t_ccode_string(t):
+    r'\"([^\\\n]|(\\.))*?\"'
+
+# C character literal
+def t_ccode_char(t):
+    r'\'([^\\\n]|(\\.))*?\''
+
+# Any sequence of non-whitespace characters (not braces, strings)
+def t_ccode_nonspace(t):
+    r'[^\s\{\}\'\"]+'
+
+# Ignored characters (whitespace)
+t_comment_ignore = " \t\n"
+
+# For bad characters, we just skip over it
+def t_comment_error(t):
+    r'.'
+    t.lexer.skip(1)
+
 # Build the lexer
 lexer = lex.lex()
 
@@ -55,7 +122,7 @@ a = 3 + 4 * 10
   + -20 *2
 '''
 
-# Giv the lexer some input
+# Give the lexer some input
 lexer.input(data)
 
 # Tokenize
